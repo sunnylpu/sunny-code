1f66b358e9ac4635ff4719a3ab1e826958722a41
sunny tyagi <143286883+sunnylpu@users.noreply.github.com>
2024-03-12 23:48:42 +0000

    adding comments in the program

--- strings/reverse_string.cpp
+++ strings/reverse_string.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 class Solution {
 public:
-
+// this is the program for reverse string
     void reverseString(vector<char>& s) {
         int st=0;
         int e = s.size()-1;
