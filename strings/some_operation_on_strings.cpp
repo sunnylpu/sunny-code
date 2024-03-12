bf6300cdca92dcb0d7261de54828a939e5c2b60a
sunny tyagi <143286883+sunnylpu@users.noreply.github.com>
2024-03-12 00:12:42 +0000

    extra lowercase function added

--- strings/some_operation_on_strings.cpp
+++ strings/some_operation_on_strings.cpp
@@ -7,12 +7,24 @@ void reversestring(char name[],int size){
         swap(name[start++],name[end--]);
     }
 }
+
+
+char tolowercase(char ch){
+    if(ch>='a' && ch<='z')
+    {
+        return ch;
+    }
+    else
+    {
+        char temp=ch - 'A' + 'a';
+    }
+}
 bool checkpalindrome(char name[],int size){
     int start=0;
     int end=size-1;
     while (start<end)
     {
-        if(name[start]!=name[end]){
+        if(tolowercase(name[start])!=tolowercase(name[end])){
             return 0;
         }
         else{
@@ -33,14 +45,23 @@ int getlength(char name[]){
 }
 int main(){
     char name[20];
+
     cout<<"enter your name"<<endl;
     cin>>name;
     int len=getlength(name);
     cout<<"length is:"<<len<<endl;
+    
+    
     reversestring(name,len);
     cout<<"your name is :"<<name<<endl;
+    
+    
     int palindrome=checkpalindrome(name,len);
     cout<<palindrome<<endl;
+
+
+    cout<<"character is :"<<tolowercase('B')<<endl;
+    cout<<"character is :"<<tolowercase('b')<<endl;
 
     return 0;
 }
