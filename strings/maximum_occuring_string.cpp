9c785259675cbec1d4dbb2e4b4c2033325f94b16
sunnylpu <sunnytyagi2004@gmail.com>
2024-03-27 20:55:46 +0530

    this is the day i feel good in the coding

---
+++ strings/maximum_occuring_string.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include <strings>
+#include<array>
+using namespace std;
+char maxOccurance(char s){
+    int arr[26]={0};
+    // create an  array of count of characters;
+    for(int i=0;i<s.length;i++){
+
+    }
+}
+int main(){
+    string s
+
+
+
+    retrun 0;
+}
