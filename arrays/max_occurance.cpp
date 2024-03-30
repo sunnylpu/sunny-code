7ebf53be43e7a9c4479c297e303d4db9e170649b
sunnylpu <sunnytyagi2004@gmail.com>
2024-03-30 22:33:01 +0530

    incomplete code for max max_occurance character

---
+++ arrays/max_occurance.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+char maxoccurance(string s){
+    int arr[26]={0};
+    for (int i = 0; i < s.length(); i++) {
+        char ch=s[i];
+        int number=0;
+        number=ch- 'a';
+        arr[number]++;
+}
+int maxi=0;
+
+
+int main(){
+    int arr[26]={0};
+
+}
