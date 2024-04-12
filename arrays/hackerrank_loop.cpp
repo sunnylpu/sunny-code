3307210f76450f2db105824effbbe8f74ff9a037
sunnylpu <sunnytyagi2004@gmail.com>
2024-04-12 21:31:01 +0530

    no Changes

--- arrays/hackerrank_loop.cpp
+++ arrays/hackerrank_loop.cpp
@@ -10,20 +10,20 @@ int main() {
             case 1: cout << "one"; break;
             case 2: cout << "two"; break;
             case 3: cout << "three"; break;
-            case 4: cout << "four"; break;
-            case 5: cout << "five"; break;
-            case 6: cout << "six"; break;
-            case 7: cout << "seven"; break;
-            case 8: cout << "eight"; break;
-            case 9: cout << "nine"; break;
+            case 4: cout << " four"; break;
+            case 5: cout << " five"; break;
+            case 6: cout << " six"; break;
+            case 7: cout << " seven"; break;
+            case 8: cout << " eight"; break;
+            case 9: cout << " nine"; break;
         }
         }
     else{
          if (n%2==0){
-            cout<<"even";
+            cout<<" even";
         }
         else{
-             cout<<"odd";
+             cout<<" odd";
         }
     }return 0;
 }
