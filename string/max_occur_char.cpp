ec501346f082cfc07e1be77ecba04dd726bbf28f
sunnylpu <sunnytyagi2004@gmail.com>
2024-06-18 14:23:42 +0530

    max_occur_char string

--- string/max_occur_char.cpp
+++ string/max_occur_char.cpp
@@ -2,8 +2,30 @@
 using namespace std;
 
 char maxoccur(string s){
-    
+    int arr[26]={0};
+
+    // create an array for count of character 
+    for(int i=0;i<s.length();i++){
+        char ch=s[i];
+        //lowercase
+        int num=0;
+        num=ch - 'a';
+        arr[num]++;
+    }
+
+        //find the max occuring character.
+    int maxi=-1,ans=0;
+    for(int i=0;i<26;i++){
+        if(maxi<arr[i]){
+            ans=i;
+            maxi=arr[i];
+        }
+    }
+    return 'a'+ ans;
 }
 int main(){
-
+    string s;
+    cin>>s;
+    cout<<maxoccur(s)<<endl;
+    return 0;
 }
