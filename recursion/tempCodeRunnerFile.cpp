24e4e9c936cef621186e9b579a30b2d490567c95
sunnylpu <sunnytyagi2004@gmail.com>
2024-06-22 07:47:09 +0530

    reverse_string code

---
+++ recursion/tempCodeRunnerFile.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+void reverse(string str,int s,int e) {
+    
+    if(s>=e){
+        return;
+    }
+    
+   swap(str[s],str[e]);
+   
+   reverse(str,s+1,e-1);
+    
+}
+int main(){
+    string str="sunny";
+    int s=0;
+    int e=str.length()-1;
+    reverse(str,s,e);
+    cout<<str<<endl;
+    return 0;
+}
