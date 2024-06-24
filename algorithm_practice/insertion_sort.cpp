68d065ca5b120f655d3428c053294032a740609b
sunnylpu <sunnytyagi2004@gmail.com>
2024-06-24 09:48:16 +0530

    complete insertion_sort code

--- algorithm_practice/insertion_sort.cpp
+++ algorithm_practice/insertion_sort.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 void insertion_sort(int *arr,int n){
-    for(int i=0;i<n;i++){
-        
+    for(int i=1;i<n;i++){
+        int temp=arr[i];
+        int j=i-1;
+        for(;j>=0;j--){
+            
+            if(arr[j]>temp){
+                //shift
+                arr[j+1]=arr[j];
+            }
+            else{
+                break;
+            }
+        }
+        arr[j+1]=temp;
     }
 }
 int main(){
