f7266d21e1858aca374ed4e4e85dc95a1a35ffc0
sunnylpu <sunnytyagi2004@gmail.com>
2024-06-27 16:00:10 +0530

    some OOPs concept

--- OOPs/intro.cpp
+++ OOPs/intro.cpp
@@ -3,11 +3,64 @@ using namespace std;
 
 class Boy{
     public:
+    int sunny;
     int health;
     int level;
-    
+
+    Boy(){
+        cout<<"constructor called"<<endl;
+    }
+
+    // parametersized constructor
+    Boy(int sunny){
+        cout<<"sunny parameeterissed constructor "<<this<<endl;
+        this -> sunny=sunny;
+    }
+
+    void print(){
+        cout<<endl;
+        cout<<"health "<<this ->health<<endl;
+        cout<<"level "<<this ->level<<endl;
+    }
 };
 int main(){
+    
+    int r(Boy);
+    cout<<&r<<endl;
+    r.print();
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // cout<<"hi "<<endl;
+    // //statically created object
+    // Boy sunny(0);
+    // cout<<"hello "<<endl;
+    // cout<<"address of "<<&sunny<<endl;
+    // // dynamically created object
+    // Boy *sun= new Boy;
+    
+
+
+
+
+
+
+
+
+
+
+
+
     // this is static allocation
     // Boy sunny;
     // sunny.health=70;
@@ -18,5 +71,5 @@ int main(){
 
 
     // dynamic allocation
-    Boy *b=new Boy;
+    //Boy *b=new Boy;
 }
