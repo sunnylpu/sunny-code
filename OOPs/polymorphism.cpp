9aa3366b28a1fa8cc4f5dd962c6a12a7750c1c1e
sunnylpu <sunnytyagi2004@gmail.com>
2024-06-28 13:15:50 +0530

    compile time polymorphism

--- OOPs/polymorphism.cpp
+++ OOPs/polymorphism.cpp
@@ -16,8 +16,15 @@ class p{
         int value2=obj.a;
         cout<<" output "<<value2-value1<<endl ;
      }
+     void operator() (){
+        cout<<"this is a ooperator overloading "<<this-> a<<endl;
+     }
 
 };
 int main(){
-    
+    p obj1,obj2;
+    obj1.a=4;
+    obj2.a=7;
+    obj1 + obj2;
+    obj1();
 }
