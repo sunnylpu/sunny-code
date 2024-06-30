a3967252e5f5abfb77ceb4d1bb740568b113ded4
sunnylpu <sunnytyagi2004@gmail.com>
2024-06-30 13:29:11 +0530

    nodeto insert at any postion complet committed

--- linked_list/doubly linked list/DLL.cpp
+++ linked_list/doubly linked list/DLL.cpp
@@ -19,7 +19,7 @@ void print(node* head){
         cout<<temp -> data<<" ";
         temp=temp -> next;
     }
-    cout<<endl;
+    
 
 }
 int getLen(node* head){
@@ -32,20 +32,59 @@ int getLen(node* head){
     return count;
 }
 
-void insertatnode(node* &head,int d){
+void insertathead(node* &head,int d){
     node* temp=new node(d);
     temp->next=head;
     head->prev=temp;
     head=temp;
 
 }
+void insertattail(node* &tail,int d){
+    node* temp=new node(d);
+    tail->next=temp;
+    temp ->prev=tail;
+    tail=temp;  
+}
+void insertatpostion(node* &head,node* &tail,int position,int d){
+     if(position==1){
+                insertathead(head,d);
+                return;
+        }
+    node* temp=head;
+    int cnt=1;
+    while(cnt<position-1){
+        temp=temp->next;
+        cnt++;
+    }
+
+    //inserting at last position
+    if(temp -> next==NULL){
+        insertattail(tail,d);
+        return;
+    }
+    // create a node for d
+    node* nodetoinsert=new node(d);
+
+    nodetoinsert -> next=temp ->next;
+    temp ->next->prev=nodetoinsert;
+    temp->next=nodetoinsert;
+    nodetoinsert -> prev=temp;
+    //temp->next=nodetoinsert;
+}
 int main(){
     node* node1=new node(10);
     node* head=node1;
+    node* tail=node1;
+    node* nodetoinsert=node1;
     print(head);
     cout<<head-> next<<endl;
     cout<<head-> prev<<endl;
 
-    insertatnode(head,11);
+    insertathead(head,11);
+    print(head);
+    insertattail(tail,12);
+    print(tail);
+
+    insertatpostion(head,tail,3,32);
     print(head);
 }
