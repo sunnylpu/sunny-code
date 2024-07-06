1a4d7c70ac4b5ec3837dafb592f5300e124ca76e
sunnylpu <sunnytyagi2004@gmail.com>
2024-07-06 15:29:43 +0530

    stack_operation without stl library

---
+++ stack/stack_operation.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include<stack>
+using namespace std;
+
+class stack1{
+    // properties
+    public:
+    int *arr;
+    int size;
+    int top;
+
+    // constructtor
+    stack1(int size){
+        this -> size=size;
+        arr = new int[size];
+        top=-1;
+    }
+
+    void push(int element){
+
+    }
+
+    void pop(int element){
+
+    }
+
+    void peak(int element){
+
+    }
+
+    void isempty(){
+        
+    }
+    
+};
+int main(){
+   
+
+}
