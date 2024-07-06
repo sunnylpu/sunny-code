57d5a2a72df4ccc1a10768d0e2c9aa99053d89aa
sunnylpu <sunnytyagi2004@gmail.com>
2024-07-06 12:33:28 +0530

    stack operation

--- linked_list/circular linked list/loop_removal_all_method.cpp
+++ linked_list/circular linked list/loop_removal_all_method.cpp
@@ -1,9 +1,103 @@
 
-
-
-
 // floyd detection cycle
+
+
+
+
+
+
+
+
+
+// #include <iostream>
+
+// class Node {
+// public:
+//     int data;
+//     Node* next;
+
+//     Node(int data) {
+//         this->data = data;
+//         this->next = NULL;
+//     }
+// };
+
+// void removeLoop(Node* loopNode, Node* head) {
+//     Node* ptr1 = head;
+//     Node* ptr2 = NULL;
+//     while (1) {
+//         ptr2 = loopNode;
+//         while (ptr2->next != loopNode && ptr2->next != ptr1) {
+//             ptr2 = ptr2->next;
+//         }
+//         if (ptr2->next == ptr1) {
+//             break;
+//         }
+//         ptr1 = ptr1->next;
+//     }
+//     ptr2->next = NULL;
+// }
+
+// bool detectAndRemoveLoop(Node* head) {
+//     if (head == NULL || head->next == NULL) {
+//         return false;
+//     }
+
+//     Node* slow = head;
+//     Node* fast = head;
+
+//     while (slow && fast && fast->next) {
+//         slow = slow->next;
+//         fast = fast->next->next;
+
+//         if (slow == fast) {
+//             removeLoop(slow, head);
+//             return true;
+//         }
+//     }
+
+//     return false;
+// }
+
+// void printList(Node* head) {
+//     while (head != NULL) {
+//         std::cout << head->data << " ";
+//         head = head->next;
+//     }
+//     std::cout << std::endl;
+// }
+
+// int main() {
+//     Node* head = new Node(1);
+//     head->next = new Node(2);
+//     head->next->next = new Node(3);
+//     head->next->next->next = new Node(4);
+//     head->next->next->next->next = new Node(5);
+
+//     // Creating a loop for testing
+//     head->next->next->next->next->next = head->next->next;
+
+//     if (detectAndRemoveLoop(head)) {
+//         std::cout << "Loop detected and removed." << std::endl;
+//     } else {
+//         std::cout << "No loop detected." << std::endl;
+//     }
+
+//     printList(head);
+
+//     return 0;
+// }
+   
+
+
+
+   // using hashing
+
+
+
+
 #include <iostream>
+#include <unordered_set>
 
 class Node {
 public:
@@ -16,40 +110,18 @@ public:
     }
 };
 
-void removeLoop(Node* loopNode, Node* head) {
-    Node* ptr1 = head;
-    Node* ptr2 = NULL;
-    while (1) {
-        ptr2 = loopNode;
-        while (ptr2->next != loopNode && ptr2->next != ptr1) {
-            ptr2 = ptr2->next;
-        }
-        if (ptr2->next == ptr1) {
-            break;
-        }
-        ptr1 = ptr1->next;
-    }
-    ptr2->next = NULL;
-}
-
 bool detectAndRemoveLoop(Node* head) {
-    if (head == NULL || head->next == NULL) {
-        return false;
-    }
-
-    Node* slow = head;
-    Node* fast = head;
-
-    while (slow && fast && fast->next) {
-        slow = slow->next;
-        fast = fast->next->next;
-
-        if (slow == fast) {
-            removeLoop(slow, head);
+    std::unordered_set<Node*> visitedNodes;
+    Node* prev = NULL;
+    while (head != NULL) {
+        if (visitedNodes.find(head) != visitedNodes.end()) {
+            prev->next = NULL;
             return true;
         }
+        visitedNodes.insert(head);
+        prev = head;
+        head = head->next;
     }
-
     return false;
 }
 
@@ -81,3 +153,15 @@ int main() {
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
