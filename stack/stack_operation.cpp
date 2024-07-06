1b35af7976843c2d6f515c91107f3de19c252f54
sunnylpu <sunnytyagi2004@gmail.com>
2024-07-06 16:18:44 +0530

    stack_operation commit

--- stack/stack_operation.cpp
+++ stack/stack_operation.cpp
@@ -1,39 +1,71 @@
 #include <iostream>
-#include<stack>
 using namespace std;
 
-class stack1{
-    // properties
-    public:
-    int *arr;
+class stack1 {
+public:
+    int* arr;
     int size;
     int top;
 
-    // constructtor
-    stack1(int size){
-        this -> size=size;
+    // Constructor
+    stack1(int size) {
+        this->size = size;
         arr = new int[size];
-        top=-1;
+        top = -1;
     }
 
-    void push(int element){
-
+    void push(int element) {
+        if (size - top > 1) { // space is available in stack for push
+            top++;
+            arr[top] = element;
+        } else {
+            cout << "stack overflow" << endl;
+        }
     }
 
-    void pop(int element){
-
+    void pop() {
+        if (top >= 0) {
+            top--;
+        } else {
+            cout << "stack underflow" << endl;
+        }
     }
 
-    void peak(int element){
-
+    int peak() {
+        if (top >= 0 && top < size) {
+            return arr[top];
+        } else {
+            cout << "stack is empty" << endl;
+            return -1;
+        }
     }
 
-    void isempty(){
-        
+    bool isempty() {
+        return top == -1;
     }
-    
 };
-int main(){
-   
 
+int main() {
+    stack1 s(5); // Creating a stack of size 5
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    cout << "Top element is: " << s.peak() << endl;
+
+    s.pop();
+    cout << "Top element after pop is: " << s.peak() << endl;
+
+    s.pop();
+    s.pop();
+    s.pop(); // This will cause underflow
+
+    if (s.isempty()) {
+        cout << "Stack is empty" << endl;
+    } else {
+        cout << "Stack is not empty" << endl;
+    }
+
+    return 0;
 }
