a5b775c7e22cda55b99ec507a909dad898e6c6a3
sunnylpu <sunnytyagi2004@gmail.com>
2024-07-17 11:48:14 +0530

    no changes

--- recursion/a_to_the_power_b.cpp
+++ recursion/a_to_the_power_b.cpp
@@ -1,32 +1,102 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int power(int a,int b){
-
-    //base case
-    if(b==0){
+int a_to_the_power_b(int base,int power){
+    if(power==0){
         return 1;
     }
-    if(b==1){
-        return a;
+    if(power==1){
+        return base;
     }
-
-    //recursive call
-
-    int ans=power(a,b/2);
-    if(b%2==0){
-        // for even power
-        return ans*ans;
-    }
-    else{
-        // for odd power
-        return a*ans*ans;
-    }
-
+    return base * a_to_the_power_b(base, power - 1);
 }
 int main(){
-    int a , b;
-    cin>>a>>b;
-    int  ans=power(a,b);
-    cout<<" a to the power b is "<<ans;
+    int base,power;
+    cin>>base>>power;
+    int answer=a_to_the_power_b(base,power);
+    cout<<answer;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+
+// int power(int a,int b){
+
+//     //base case
+//     if(b==0){
+//         return 1;
+//     }
+//     if(b==1){
+//         return a;
+//     }
+
+//     //recursive call
+
+//     int ans=power(a,b/2);
+//     if(b%2==0){
+//         // for even power
+//         return ans*ans;
+//     }
+//     else{
+//         // for odd power
+//         return a*ans*ans;
+//     }
+
+// }
+// int main(){
+//     int a , b;
+//     cin>>a>>b;
+//     int  ans=power(a,b);
+//     cout<<" a to the power b is "<<ans;
+// }
