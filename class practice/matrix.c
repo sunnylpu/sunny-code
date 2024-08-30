b5e8314f74c886049fcbd1ea7ec9d35c8e907e28
sunnylpu <sunnytyagi2004@gmail.com>
2024-08-30 13:27:16 +0530

    comm the main problem for matrix

---
+++ class practice/matrix.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int matrix[3][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12}
+    };
+
+    int rows = 3;
+    int cols = 4;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
