6f083387c820bee83c44992afd1a39323c3163b1
sunnylpu <sunnytyagi2004@gmail.com>
2025-01-11 10:37:05 +0530

    array methods in javascript

--- web development/backend development/array.js
+++ web development/backend development/array.js
@@ -1,9 +1,17 @@
-let arr=[1,2,3,4,5];
+// let arr=[1,2,3,4,5];
 
-let brr=new Array('sunny',2004,'tyagi');
-console.log(brr);
-console.log(arr);
-console.log(typeof(arr));
-console.log(typeof(brr));
-brr.unshift('lovely professonal university');
-console.log(brr);
+// let brr=new Array('sunny',2004,'tyagi');
+// console.log(brr);
+// console.log(arr);
+// console.log(typeof(arr));
+// console.log(typeof(brr));
+// brr.unshift('lovely professonal university');
+// console.log(brr);
+
+let arr=[1,2,3,4,5];
+arr.map(
+    (Number)=>{
+        console.log(Number**2);
+    }
+
+);
