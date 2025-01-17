a5deb9e0f08f3a606cd845e22231398e3d59cc9d
sunnylpu <sunnytyagi2004@gmail.com>
2025-01-17 22:02:21 +0530

    no_17

---
+++ c++ programming/striver dsa practice/pattern/no_17.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void alphahill(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        char num='A';
+        for(int j=0;j<2*i+1;j++){
+            cout<<num<<" ";
+            num=num+1;
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    alphahill(n);
+}
