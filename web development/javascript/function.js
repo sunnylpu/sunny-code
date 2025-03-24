f29453c9c0954f08944881eb836aa4cfac95c257
sunnylpu <sunnytyagi2004@gmail.com>
2025-03-24 10:41:12 +0530

    no changes

---
+++ web development/javascript/function.js
@@ -0,0 +1,11 @@
+// function greet(name) {
+//     return "hello " + name + "! ";
+// }
+
+// console.log(greet("sunny"));
+
+
+const greet=function(name){
+    return "hello " + name + "! ";
+}
+console.log(greet("sunny"));
