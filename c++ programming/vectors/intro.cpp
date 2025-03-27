d2043a427d0e98f239f7efcb9016125ab068610f
sunnylpu <sunnytyagi2004@gmail.com>
2025-03-27 10:10:43 +0530

    no changes

--- c++ programming/vectors/intro.cpp
+++ c++ programming/vectors/intro.cpp
@@ -1,16 +1,14 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 int main(){
-    int n;
+    vector<int> arr1;
+    cout<<"size is "<<arr1.capacity();
     cin>>n;
-    vector<int> arr;
-    for(int i=0;i<n;i++){
-        arr.push_back(i);
+    cout<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        
     }
-
-    for(int i=0;i<n;i++){
-        cout<<arr[i];
-    }
+    
 }
-
